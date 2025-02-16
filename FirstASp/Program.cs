f5a69573cceee20b738aa16fceb82f31ab819425
MUHAMMAD LUQMAN BIN ABDUL RAHMAN <88386088+muhdluqman@users.noreply.github.com>
2025-02-16 23:58:06 +0800

    Initial commit

---
+++ FirstASp/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using FirstASp.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DBContextASPConnection") ?? throw new InvalidOperationException("Connection string 'DBContextASPConnection' not found.");
+
+builder.Services.AddDbContext<DBContextASP>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<SampleUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DBContextASP>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
